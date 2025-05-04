b5f90f6366fd63501a96a06efe8cf1775d0dc4d1
kabirajpan <kabirajpan2@gmail.com>
2025-05-04 11:19:13 +0530

    Java Day one

---
+++ day01/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Sum of these numbers is: ");
+
+        int num1 = 10;
+        int num3 = 5;
+        int num2 = 6;
+        int sum =  num1 + num2 + num3;
+        
+        System.out.print(sum);
+
+    }
+}
