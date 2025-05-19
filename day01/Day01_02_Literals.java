3c7f720994496736a1a1fe4e03edf2db9d8201b3
Kabirajpan <kabirajpan2@gmail.com>
2025-05-19 14:25:40 +0530

    Day2 repository added

---
+++ day01/Day01_02_Literals.java
@@ -0,0 +1,10 @@
+public class Day01_02_Literals {
+    public static void main(String[] args){
+        byte age = 24;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.55d;
+        
+        System.out.print(age + " " + ch + " " + f1 + " " + d1);
+    }
+}
---
+++ day02/BasicSum.java
@@ -0,0 +1,6 @@
+
+public class BasicSum {
+    public static void main(String[] arg){
+        System.out.println("hello Worl");
+    }
+}
